0fc542abdf68cda72d0d695b83be06f4d7c42002
woods.jpeg <88164706+shubhambhatia2103@users.noreply.github.com>
2024-07-08 22:39:56 +0530

    Time: 348 ms (67.11%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,4 @@
-SELECT name
-FROM Employee
-WHERE id in (
-  SELECT managerId
-  FROM Employee
-  GROUP BY managerId
-  HAVING COUNT(managerId) >= 5
-)
+# Write your MySQL query statement below
+
+SELECT name from Employee where id in
+(SELECT managerId FROM Employee group by managerID having (COUNT(DISTINCT ID)) >=5)
